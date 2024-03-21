e52e6f45a6be309d58ce29f9d227f0c1b6a717bd
Vishwa <vishwa.m2204@gmail.com>
2024-03-21 17:47:37 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,18 @@ function reveal() {
 const observer = new IntersectionObserver(reveal, options);
 observer.observe(reveals);
 
+
+
+// <!-- FAQ'S  -->
+
+const buttons = document.querySelectorAll("#button");
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+})
